85ea860e57c8209508aa1e375025d70a665a4b4f
Shiv <info.techieshiv@gmail.com>
2022-06-30 12:20:48 +0530

    Front-page update

--- front-page.php
+++ front-page.php
@@ -11,39 +11,39 @@ get_header();
             foreach (get_posts(array('numberposts' => 10)) as $post) :
                 if (!empty(get_the_post_thumbnail_url($post->ID))) :
             ?>
-                    <div class="inner-banner">
-                        <div class="banner-img">
-                            <a href="<?php echo get_permalink($post->ID); ?>">
-                                <img src=" <?php echo get_the_post_thumbnail_url($post->ID); ?>" alt="">
-                            </a>
+            <div class="inner-banner">
+                <div class="banner-img">
+                    <a href="<?php echo get_permalink($post->ID); ?>">
+                        <img src=" <?php echo get_the_post_thumbnail_url($post->ID); ?>" alt="">
+                    </a>
+                </div>
+                <div class="banner-text">
+                    <a href="<?php echo get_category_link(get_the_category($post->ID)[0]); ?>">
+                        <h3><?php echo get_the_category($post->ID)[0]->name; ?></h3>
+                    </a>
+                    <a href="<?php echo get_permalink($post->ID); ?>">
+                        <h1><?php echo $post->post_title; ?></h1>
+                    </a>
+                    <div class="comment d-flex">
+                        <div class="d-flex align-items-center comment1">
+                            <div class="flex-shrink-0">
+                                <span class="icon icon-chatting"></span>
+                            </div>
+                            <div class="flex-grow-1 ms-3">
+                                <?php echo $post->comment_count; ?> Comments
+                            </div>
                         </div>
-                        <div class="banner-text">
-                            <a href="<?php echo get_category_link(get_the_category($post->ID)[0]); ?>">
-                                <h3><?php echo get_the_category($post->ID)[0]->name; ?></h3>
-                            </a>
-                            <a href="<?php echo get_permalink($post->ID); ?>">
-                                <h1><?php echo $post->post_title; ?></h1>
-                            </a>
-                            <div class="comment d-flex">
-                                <div class="d-flex align-items-center comment1">
-                                    <div class="flex-shrink-0">
-                                        <span class="icon icon-chatting"></span>
-                                    </div>
-                                    <div class="flex-grow-1 ms-3">
-                                        <?php echo $post->comment_count; ?> Comments
-                                    </div>
-                                </div>
-                                <div class="d-flex align-items-center comment1">
-                                    <div class="flex-shrink-0">
-                                        <span class="icon icon-user"></span>
-                                    </div>
-                                    <div class="flex-grow-1 ms-3">
-                                        <?php echo get_the_author_meta('display_name', $post->post_author); ?>
-                                    </div>
-                                </div>
+                        <div class="d-flex align-items-center comment1">
+                            <div class="flex-shrink-0">
+                                <span class="icon icon-user"></span>
+                            </div>
+                            <div class="flex-grow-1 ms-3">
+                                <?php echo get_the_author_meta('display_name', $post->post_author); ?>
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
             <?php
                 endif;
             endforeach;
@@ -53,7 +53,99 @@ get_header();
 </Section>
 
 <!-- Trending -->
-<Section class="trending"></Section>
+<section class="trending">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-6 col-lg-6">
+                        <div class="section-title d-flex align-items-center">
+                            <img src="<?php echo get_template_directory_uri() . '/assets/images/bullet.png'; ?>">
+                            <h2>Trending</h2>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <ul class="nav block-tab nav-pills mb-3 justify-content-end" id="pills-tab" role="tablist">
+                            <?php
+                            // Get all categories
+                            $categories = get_categories();
+                            foreach ($categories as $category) :
+                                if ($category->name != 'Uncategorized') :
+                                    
+                        ?>
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link" id="<?php echo $category->name; ?>-tab" data-bs-toggle="pill"
+                                    data-bs-target="#<?php echo $category->name; ?>" type="button" role="tab"
+                                    aria-controls="<?php echo $category->name; ?>"
+                                    aria-selected="true"><?php echo $category->name; ?> </button>
+                            </li>
+                            <?php
+                                endif;
+                            endforeach;
+                        ?>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="tab-content" id="pills-tabContent">
+                <?php
+                        // Get all categories
+                        $categories = get_categories();
+                        foreach($categories as $category) :
+                            if ($category->name != 'Uncategorized') :
+                                $args = array(
+                                    'post_type' => 'post',
+                                    'posts_per_page' => 3,
+                                    'category_name' => $category->name,
+                                    'orderby' => 'date',
+                                    'order' => 'DESC'
+                                );
+                        ?>
+                <div class="tab-pane fade" id="<?php echo $category->name; ?>" role="tabpanel"
+                    aria-labelledby="<?php echo $category->name; ?>-tab">
+                    <div class="row">
+                        <?php
+                        $posts = get_posts($args);
+                        foreach ($posts as $post) :
+                            if (!empty(get_the_post_thumbnail_url($post->ID))) :
+                        ?>
+                        <div class="col-md-6 col-lg-6">
+                            <div class="d-flex align-items-center terending-block">
+                                <div class="flex-shrink-0">
+                                    <a href="<?php echo get_permalink($post->ID); ?>">
+                                        <img src=" <?php echo get_the_post_thumbnail_url($post->ID); ?>" alt="">
+                                    </a>
+                                </div>
+                                <div class="flex-grow-1 ms-3">
+                                    <div class="comment d-flex">
+                                        <div class="d-flex align-items-center  comment1">
+                                            <div class="flex-shrink-0">
+                                                <span class="icon icon-chatting"></span>
+                                            </div>
+                                            <div class="flex-grow-1 ms-3">
+                                                <?php echo $post->comment_count; ?> Comments
+                                            </div>
+                                        </div>
+                                        <div class="d-flex align-items-center  comment1">
+                                            <div class="flex-shrink-0">
+                                                <span class="icon icon-user"></span>
+                                            </div>
+                                            <div class="flex-grow-1 ms-3">
+                                                <?php echo get_the_author_meta('display_name', $post->post_author); ?>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <h5><?php echo $post->post_title; ?></h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
 
 <!-- World -->
 <Section class="trending world"></Section>
--- single.php
+++ single.php
@@ -5,82 +5,92 @@ shopify_aid_get_post_views(get_the_ID());
 while (have_posts()) :
     the_post();
 ?>
-    <!-- Post Thumbnail with Title -->
-    <section class="small-banner single-banner">
-        <div class="container-fluid">
-            <div class="small-inner" style="background: url('<?php echo get_the_post_thumbnail_url(); ?>')no-repeat; background-size:cover">
-                <h1><?php the_title(); ?></h1>
-            </div>
+<!-- Post Thumbnail with Title -->
+<section class="small-banner single-banner">
+    <div class="container-fluid">
+        <div class="small-inner"
+            style="background: url('<?php echo get_the_post_thumbnail_url(); ?>')no-repeat; background-size:cover">
+            <h1><?php the_title(); ?></h1>
         </div>
-    </section>
+    </div>
+</section>
 
-    <!-- Post Content -->
-    <section class="morbie-felis">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-12 col-md-12">
-                    <div class="morbie-outer">
-                        <?php the_content(); ?>
-                    </div>
+<!-- Post Content -->
+<section class="morbie-felis">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-12 col-md-12">
+                <div class="morbie-outer">
+                    <?php the_content(); ?>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-    <!-- Related Posts -->
-    <section class="spotlight related-post">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-12">
-                    <div class="section-title-2">
-                        <h2>Related Posts <span></span></h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br> Lorem Ipsum has been the industry's standard dummy.</p>
-                        <?php
+<!-- Related Posts -->
+<section class="spotlight related-post">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="section-title-2">
+                    <h2>Related Posts <span></span></h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br> Lorem Ipsum has
+                        been the industry's standard dummy.</p>
+                    <?php
                         $related_posts = get_posts(array(
+                            // Posts only has thumbnails
                             'post_type' => 'post',
-                            'posts_per_page' => 9,
+                            'post_status' => 'publish',
+                            'posts_per_page' => 4,
                             'post__not_in' => array(get_the_ID()),
+                            'orderby' => 'rand',
                             'tax_query' => array(
                                 array(
                                     'taxonomy' => 'category',
                                     'field' => 'term_id',
-                                    'terms' => wp_get_post_categories(get_the_ID())
+                                    'terms' => wp_get_post_categories(get_the_ID(), array('fields' => 'ids'))
                                 )
                             )
+
                         ));
                         ?>
-                        <div class="section-btn">
-                            <a class="customm-btn" href="<?php echo get_permalink(get_option('page_for_posts')); ?>">View All</a>
-                        </div>
+                    <div class="section-btn">
+                        <a class="customm-btn" href="<?php echo get_permalink(get_option('page_for_posts')); ?>">View
+                            All</a>
                     </div>
                 </div>
-                <?php
+            </div>
+            <?php
                 foreach ($related_posts as $post) :
                     if (!empty(get_the_post_thumbnail_url($post->ID))) :
                 ?>
-                        <div class="col-md-6 col-lg-4">
-                            <div class="spotlight-box">
-                                <div class="spotlight-img">
-                                    <a href="<?php echo get_permalink($post->ID); ?>">
-                                        <img src="<?php echo get_the_post_thumbnail_url($post->ID); ?>" alt="" class="img-fluid">
-                                    </a>
-                                </div>
-                                <div class="box-date">
-                                    <a href=""><?php echo get_the_date(__('M d, Y'), $post->ID); ?></a>
-                                    <a href=""><?php echo ucfirst(get_the_author_meta('display_name', $post->post_author)); ?></a>
-                                </div>
-                                <h3><?php echo $post->post_title; ?></h3>
-                                <p><?php echo wp_trim_words($post->post_content, 25); ?></p>
-                            </div>
-                        </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="spotlight-box">
+                    <div class="spotlight-img">
+                        <a href="<?php echo get_permalink($post->ID); ?>">
+                            <img src="<?php echo get_the_post_thumbnail_url($post->ID); ?>" alt="" class="img-fluid">
+                        </a>
+                    </div>
+                    <div class="box-date">
+                        <a href=""><?php echo get_the_date(__('M d, Y'), $post->ID); ?></a>
+                        <a href=""><?php echo ucfirst(get_the_author_meta('display_name', $post->post_author)); ?></a>
+                    </div>
+                    <!-- Limit Title Characters -->
+                    <a href="<?php echo get_permalink($post->ID); ?>">
+                        <h3><?php echo wp_trim_words($post->post_title, 5, '...'); ?></h3>
+                    </a>
+                    <p><?php echo wp_trim_words($post->post_content, 20); ?></p>
+                </div>
+            </div>
 
-                <?php
+            <?php
                     endif;
                 endforeach;
                 ?>
-            </div>
         </div>
-    </section>
+    </div>
+</section>
 
 
 <?php
